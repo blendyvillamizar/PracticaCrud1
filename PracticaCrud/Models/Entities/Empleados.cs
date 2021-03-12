e8728db5e17527a7c7a116bca8cd5cd7d336c76c
Your Name <you@example.com>
2021-03-11 23:13:41 -0500

    Se agrega la clase empleado

--- PracticaCrud/Models/Entities/Empleados.cs
+++ PracticaCrud/Models/Entities/Empleados.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,12 @@ namespace PracticaCrud.Models.Entities
         [Key]
         public int EmpleadoId { get; set; }
 
+        [Column("Nombre Empleado", TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage ="El nombre es requerido")]
         public string Nombre { get; set; }
-
-        public string Nombre { get; set; }
+       
+        public int Documento { get; set; }
+        public int CargoId { get; set; }
+        public string Telefono { get; set; }
     }
 }

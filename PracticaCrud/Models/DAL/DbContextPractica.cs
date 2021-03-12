ea8894b6fc324d0987988cffde100b48e3dbf4e2
Your Name <you@example.com>
2021-03-11 22:55:41 -0500

    Agregar archivos de proyecto.

---
+++ PracticaCrud/Models/DAL/DbContextPractica.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaCrud.Models.DAL
+{
+    public class DbContextPractica
+    {
+    }
+}
